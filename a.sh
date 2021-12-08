ac4f5174fa789ad1311f47622ac0f91982570cad
小东 <49069586+x-ddr@users.noreply.github.com>
2021-12-08 23:24:06 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,1 +1,1 @@
-docker run -tid --name baota --net=host --privileged=true --shm-size=1g --restart always -v ~/wwwroot:/www/wwwroot pch18/baota
+docker run -tid --name baota --net=host --privileged=true --shm-size=1g --restart always -v $PWD:/www/wwwroot pch18/baota
